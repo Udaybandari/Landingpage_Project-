043516a0eb89fd7b056167826f5d5f76d7b5a1a1
Uday bandari <udaybandari593@gmail.com>
2025-07-10 20:10:45 +0530

    added new feature responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Sections/Header"
 
 
 function App() {
  
   return (
-  <div>
-    <h1 className="text-cyan-300 text-6xl">Welcome</h1>
-  </div>
+<main className="overflow-hidden">
+  <Header/>
+</main>
   )
 }
 
