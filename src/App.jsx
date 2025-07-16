1dab442ee8ea2a18fa55d6d5cf11688d76acb57d
Uday bandari <udaybandari593@gmail.com>
2025-07-16 20:23:15 +0530

    added another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Sections/Features"
 import Header from "./components/Sections/Header"
 import Hero from "./components/Sections/Hero"
+import Pricing from "./components/Sections/Pricing"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   <Header/>
   <Hero/>
   <Features/>
+  <Pricing/>
 </main>
   )
 }
